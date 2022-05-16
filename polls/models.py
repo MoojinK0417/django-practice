82e5856bc60a9e43002073ca129298ecc9fdfde7
Moojin Kim <mujin990417@gmail.com>
2022-05-16 02:18:49 -0400

    second commit

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,9 @@
-from time import time
-from xml.dom.pulldom import default_bufsize
-from django.db import models
-from django.utils import timezone
 import datetime
 
+from django.db import models
+from django.utils import timezone
 
-
+# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -14,7 +12,11 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -23,5 +25,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-# Create your models here.
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 # from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 from .models import Question, Choice
 
@@ -33,7 +34,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
